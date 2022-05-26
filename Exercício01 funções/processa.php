3e613eede1b733b84bc36f5b129ebc0729225702
GustavoSilverio <80285864+GustavoSilverio@users.noreply.github.com>
2022-05-26 09:09:15 -0300

    Exercícios de graus

---
+++ Exercício01 funções/processa.php
@@ -0,0 +1,11 @@
+<?php
+$convt = $_POST['convt'];
+$num = $_POST['num'];
+if($convt == 1 || $convt == 2){
+    echo"$convt <br>";
+}else{
+    echo"O número está fora do que foi solicitado";
+}
+
+function c_f()
+?>
